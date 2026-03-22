009a5a847ad2cf21805727db1de1329e5676b402
Lucas-linux <lucas.r.aluno@gmail.com>
2026-03-22 14:53:37 -0300

    :recycle: refactor: troca de Ulids para Uuids

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
-    use HasFactory, HasUlids;
+    use HasFactory, HasUuids;
 
     protected $fillable = [
         'name'
